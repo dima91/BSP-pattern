bccdd5c6dace79f99e6d0631f5a41e44e62a0b1d
Luca <dima.91@live.it>
2019-06-04 18:30:30 +0200

    Removed implementation of secondo model and added PerformanceEvaluator program

--- tests/bspTester.cpp
+++ tests/bspTester.cpp
@@ -6,12 +6,11 @@
 #include <algorithm>
 #include <random>
 
-//#define BSP_TESTER_0
-//#define BSP_TESTER_1
-#define PAR_SUPERSTEP
+#define SIMPLE_TESTER
+//#define BSP_DIFFERENT_ACTIVITIES_NUMBER
 
 
-#ifdef BSP_TESTER_0
+#ifdef SIMPLE_TESTER
 
 
 using IntActivityFunction			= Superstep<int>::ActivityFunction;
@@ -74,9 +73,9 @@ int main (int argn, char **argv) {
 		s1->addActivity (bFun, bComFun);
 	}
 
-	s0->setAtExitFunction ([] (std::vector<LockableVector<int>> &outV) {
+	/*s0->setAtExitFunction ([] (std::vector<LockableVector<int>> &outV) {
 		return -1;
-	});
+	});*/
 	/*s1->setAtExitFunction ([] (std::vector<LockableVector<int>> &outV) {
 		return 0;
 	});*/
@@ -126,7 +125,7 @@ int main (int argn, char **argv) {
 
 
 
-#ifdef BSP_TESTER_1
+#ifdef BSP_DIFFERENT_ACTIVITIES_NUMBER
 
 
 #define TISKIN_PRINT_V(lbl, vec, footer) {\
@@ -268,123 +267,9 @@ int main (int argn, char **argv) {
 
 	bspPattern.runAndWait (bspInputs, bspOutputs, false);
 
-	std::cout << std::endl << std::endl << bspOutputs.size () << std::endl;
+	std::cout << "Output size: " << bspOutputs.size () << std::endl;
 
 	std::cout << "\nBye bye!\n";
-
-	return 0;
-}
-
-
-#endif
-
-
-
-
-#ifdef PAR_SUPERSTEP
-
-using IntActivityFunction			= Superstep<int>::ActivityFunction;
-using IntCommunicationProtocol		= Superstep<int>::CommunicationProtocols;
-using IntCommunicationProtocolFun	= std::function<Superstep<int>::CommunicationProtocols (int, std::vector<int> &)>;
-
-
-
-int main (int argn, char **argv) {
-	std::cout << "Hello user!\n";
-
-	BSP<int> bspPattern;
-
-	BSP<int>::SuperstepPointer s0	= BSP<int>::SuperstepPointer (new Superstep<int> ());
-	BSP<int>::SuperstepPointer s1	= BSP<int>::SuperstepPointer (new Superstep<int> ());
-
-
-	for (int i=0; i<4; i++) {
-		// ==============
-		// First activity
-		IntActivityFunction aFun	= IntActivityFunction ([] (int actIdx, std::vector<int> &inputs) {
-			for (size_t i=0; i<inputs.size(); i++) {
-				inputs[i]	= pow (inputs[i], 2);
-			}
-			return ;
-		});
-		auto aComFun	= IntCommunicationProtocolFun ([i] (int actIdx, std::vector<int> els) {
-				std::random_device random_device;
-				std::mt19937 engine{random_device()};
-				std::uniform_int_distribution<int> dist(1000, 1001);
-				//std::this_thread::sleep_for (std::chrono::milliseconds (dist(engine)));
-
-				IntCommunicationProtocol cp (4);
-				cp[(i+1)%4] = {els[0], els[1]};
-
-				return cp;
-		});
-		s0->addActivity (aFun, aComFun);
-
-
-		// ===============
-		// Second activity
-		IntActivityFunction bFun	= IntActivityFunction ([] (int actIdx, std::vector<int> &inputs) {
-			for (size_t i=0; i<inputs.size(); i++) {
-				inputs[i]	= pow (inputs[i], 0.5);
-			}
-			return ;
-		});
-		auto bComFun	= IntCommunicationProtocolFun ([i] (int actIdx, std::vector<int> els) {
-			std::random_device random_device;
-			std::mt19937 engine{random_device()};
-			std::uniform_int_distribution<int> dist(100, 300);
-			//std::this_thread::sleep_for (std::chrono::milliseconds (dist(engine)));
-
-			IntCommunicationProtocol cp (4);
-			cp[(i-1+4)%4] = {els[0], els[1]};
-
-			return cp;
-		});
-		s1->addActivity (bFun, bComFun);
-	}
-
-	s0->setAtExitFunction ([] (std::vector<LockableVector<int>> &outV) {
-		return -1;
-	});
-	/*s1->setAtExitFunction ([] (std::vector<LockableVector<int>> &outV) {
-		return 0;
-	});*/
-
-	std::cout << "Superstep id:  " << bspPattern.addSuperstep (std::move(s0)) << std::endl;
-	std::cout << "Superstep id:  " << bspPattern.addSuperstep (std::move(s1)) << std::endl;
-
-
-
-	std::vector<std::vector<int>>  inputVectors (4),  outputVectors (4);
-
-	inputVectors[0].push_back (1);
-	inputVectors[0].push_back (2);
-	inputVectors[1].push_back (3);
-	inputVectors[1].push_back (4);
-	inputVectors[2].push_back (5);
-	inputVectors[2].push_back (6);
-	inputVectors[3].push_back (7);
-	inputVectors[3].push_back (8);
-
-	std::cout << "Starting input vectors..\n";
-	for (size_t i=0; i<inputVectors.size(); i++) {
-		std::cout << i << " >  ";
-		for (auto el : inputVectors[i])
-			std::cout << el << "\t";
-		std::cout << std::endl;
-	}
-
-	bspPattern.runAndWait (inputVectors, outputVectors, false);
-
-
-
-	std::cout << "Output vectors..\n";
-	for (size_t i=0; i<outputVectors.size(); i++) {
-		std::cout << i << " >  ";
-		for (auto el : outputVectors[i])
-			std::cout << el << "\t";
-		std::cout << std::endl;
-	}
 
 	return 0;
 }
