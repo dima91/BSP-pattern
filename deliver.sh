bb0564c63e816079c071d840cb37310bd4b6c703
Luca <dima.91@live.it>
2019-06-17 16:17:16 +0200

    Updated deliver.sh script

--- deliver.sh
+++ deliver.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-zip -T spm19-dimauro CMakeLists.txt *.pdf include/* src/*
+ARCHIVE_NAME="spm19-dimauro"
+INCLUDE_FILES="include/barrier.hpp include/bsp.hpp include/lockableVector.hpp include/superstep.hpp include/uTimer.hpp include/workerThread.hpp"
+SOURCE_FILES="src/barrier.cpp src/tiskin.cpp src/workerThread.cpp"
+TESTS_FILES="tests/bspTester.cpp"
+
+rm $ARCHIVE_NAME.zip
+
+zip -T $ARCHIVE_NAME CMakeLists.txt ../SPM_report.pdf $INCLUDE_FILES $SOURCE_FILES $TESTS_FILES
