a085452ad8849b40f46f14f7716170a5f84213b7
Luca <dima.91@live.it>
2019-07-21 11:38:20 +0200

    Added gnuplot sources and data

---
+++ with_integrations/results/extractor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# != 1 ] ; then
+	echo "usage: extractor.sh <filename>"
+	exit
+fi
+
+if [ -f tmpValsExtractor ] ; then 
+	rm tmpValsExtractor
+fi
+
+grep "overhead" $1 | grep -Eo '[0-9\.]+' > tmpValsExtractor
+SUM=$(awk '{ sum += $1 } END { print sum }' tmpValsExtractor )
+
+echo $SUM
+echo $(($SUM/5))
+
+
+rm tmpValsExtractor
